4833170b372b387cfce68eeece5ed5b86418313e
Nobodip Debnath <nobodipdebnath45@gmail.com>
2025-12-30 12:43:00 +0600

    add buttion style

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -2,59 +2,99 @@
 import Link from "next/link";
 import { FaBars } from "react-icons/fa";
 
-
 function Navbar() {
   return (
     <nav className="bg-transparent">
       <div className="flex items-center justify-between py-5">
         <div className="flex hidden lg:block flex-shrink-0 items-center">
-          <Link
-            href="/"
-            className=" text-[#16f2b3]  text-3xl font-bold">
+          <Link href="/" className=" text-[#16f2b3]  text-3xl font-bold">
             Nobodip Debnath
           </Link>
         </div>
         <div className="flex lg:hidden flex-shrink-0 items-center">
-          <Link
-            href="/"
-            className=" text-[#16f2b3]  text-3xl font-bold">
-            Nobodip 
+          <Link href="/" className=" text-[#16f2b3]  text-3xl font-bold">
+            Nobodip
           </Link>
         </div>
-        <div>
-            <div>
-              <FaBars className="text-2xl"/>
-            </div>
+        <div className="md:hidden">
+          <div>
+            <FaBars className="text-2xl" />
+          </div>
         </div>
 
-        <ul className="mt-4 hidden lg:block flex h-screen max-h-0 w-full flex-col items-start text-sm opacity-0 md:mt-0 md:h-auto md:max-h-screen md:w-auto md:flex-row md:space-x-1 md:border-0 md:opacity-100 " id="navbar-default">
-          <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#about">
-              <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">ABOUT</div>
-            </Link>
-          </li>
-          <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#experience"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EXPERIENCE</div></Link>
-          </li>
-          <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#skills"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">SKILLS</div></Link>
-          </li>
-          <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#education"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">EDUCATION</div></Link>
-          </li>
-          <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">PROJECTS</div></Link>
-          </li>
-          {/* <li>
+        <div className="hidden md:block">
+          <ul
+            className="mt-4  flex h-screen max-h-0 w-full flex-col items-start text-sm opacity-0 md:mt-0 md:h-auto md:max-h-screen md:w-auto md:flex-row md:space-x-1 md:border-0 md:opacity-100 "
+            id="navbar-default"
+          >
+            <li>
+              <Link
+                className="block px-4 py-2 no-underline outline-none hover:no-underline"
+                href="/#about"
+              >
+                <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+                  ABOUT
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="block px-4 py-2 no-underline outline-none hover:no-underline"
+                href="/#experience"
+              >
+                <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+                  EXPERIENCE
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="block px-4 py-2 no-underline outline-none hover:no-underline"
+                href="/#skills"
+              >
+                <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+                  SKILLS
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="block px-4 py-2 no-underline outline-none hover:no-underline"
+                href="/#education"
+              >
+                <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+                  EDUCATION
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="block px-4 py-2 no-underline outline-none hover:no-underline"
+                href="/#projects"
+              >
+                <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+                  PROJECTS
+                </div>
+              </Link>
+            </li>
+            {/* <li>
             <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#projects"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">BLOG</div></Link>
           </li> */}
-          <li>
-            <Link className="block px-4 py-2 no-underline outline-none hover:no-underline" href="/#contact"><div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">CONTACT</div></Link>
-          </li>
-        </ul>
+            <li>
+              <Link
+                className="block px-4 py-2 no-underline outline-none hover:no-underline"
+                href="/#contact"
+              >
+                <div className="text-sm text-white transition-colors duration-300 hover:text-pink-600">
+                  CONTACT
+                </div>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
